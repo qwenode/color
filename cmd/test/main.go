542b153baf8854c6938082f59d68a9991b29924f
Node <8974108+qwenode@users.noreply.github.com>
2023-08-25 13:45:04 +0800

    setOutput

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,12 +1,15 @@
 package main
 
-import "github.com/qwenode/color"
+import (
+	"github.com/qwenode/color"
+	"log"
+	"strings"
+)
 
 func main() {
 	color.EnableTimePrefix()
-	color.SuccessMessage("success")
-	color.WarningMessage("warning")
-	color.InfoMessage("info")
-	color.InfoBlackMessage("info")
-	color.ErrorMessage("error")
+	color.SetOutputFile("a.log", log.Lshortfile)
+	color.SuccessMessage("哟西,%s", "bbb")
+	aa := "wowifw%s"
+	color.SuccessMessage(strings.ReplaceAll(aa, "%s", "[%s]"), "xx")
 }
--- msg.go
+++ msg.go
@@ -1,37 +1,75 @@
 package color
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"strings"
 	"time"
 )
 
 var enableTimePrefix = false
 var prefixAddition = ""
-func EnableTimePrefix(additionPrefix... string) {
+var enableWriteToFile = false
+var fileHandle *os.File
+
+func SetOutputFile(logFile string, flag int) error {
+	var err error
+	fileHandle, err = os.OpenFile(logFile, os.O_CREATE|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	enableWriteToFile = true
+	log.SetOutput(fileHandle)
+	log.SetFlags(flag)
+	return nil
+}
+func EnableTimePrefix(additionPrefix ...string) {
 	if len(additionPrefix) > 0 {
-		prefixAddition=strings.Join(additionPrefix,"")
+		prefixAddition = strings.Join(additionPrefix, "")
 	}
 	enableTimePrefix = true
 }
 func SuccessMessage(format string, a ...interface{}) {
-	colorPrint(getTimePrefix()+format, FgHiGreen, a...)
+	sprintf := getTimePrefix() + fmt.Sprintf(format, a...)
+	if enableWriteToFile {
+		log.Output(2, sprintf)
+	}
+	colorPrint(sprintf, FgHiGreen)
 }
+
 func ErrorMessage(format string, a ...interface{}) {
-	colorPrint(getTimePrefix()+format, FgHiRed, a...)
+	sprintf := getTimePrefix() + fmt.Sprintf(format, a...)
+	if enableWriteToFile {
+		log.Output(2, sprintf)
+	}
+	colorPrint(sprintf, FgHiRed)
 }
 func WarningMessage(format string, a ...interface{}) {
-	colorPrint(getTimePrefix()+format, FgHiYellow, a...)
+	sprintf := getTimePrefix() + fmt.Sprintf(format, a...)
+	if enableWriteToFile {
+		log.Output(2, sprintf)
+	}
+	colorPrint(sprintf, FgHiYellow)
 }
 func InfoMessage(format string, a ...interface{}) {
-	colorPrint(getTimePrefix()+format, FgWhite, a...)
+	sprintf := getTimePrefix() + fmt.Sprintf(format, a...)
+	if enableWriteToFile {
+		log.Output(2, sprintf)
+	}
+	colorPrint(sprintf, FgWhite)
 }
 func InfoBlackMessage(format string, a ...interface{}) {
-	colorPrint(getTimePrefix()+format, FgBlack, a...)
+	sprintf := getTimePrefix() + fmt.Sprintf(format, a...)
+	if enableWriteToFile {
+		log.Output(2, sprintf)
+	}
+	colorPrint(sprintf, FgBlack)
 }
 func getTimePrefix() string {
 	if !enableTimePrefix {
 		return ""
 	}
-	s:= time.Now().Format("[2006/01/02 15:04:05] ")
-	return s+prefixAddition
+	s := time.Now().Format("[2006/01/02 15:04:05] ")
+	return s + prefixAddition
 }
